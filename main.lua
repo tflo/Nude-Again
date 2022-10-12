aaa8c3879c2a112f3604a57b0c3d7d3fd633e366
tflo <thfl@mac.com>
2022-10-12 03:20:28 +0200

    Tested StyLua on this file

--- main.lua
+++ main.lua
@@ -1,25 +1,37 @@
 
-local sensitiveSlots, allSlots, items, freeslots = {1, 3, 5, 6, 7, 8, 9, 10, 16, 17}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19}, {}, {}
-local msgNude, msgFullNude, msgClothed, msgFailure = "Nudist: Almost nude now!", "Nudist: Really nude now!", "Nudist: Equipped again. Probably.", "Nudist: |cffFF0000Something went wrong while trying to reequip your weapon slots. Check your equipment!"
+local sensitiveSlots, allSlots, items, freeslots =
+	{ 1, 3, 5, 6, 7, 8, 9, 10, 16, 17 },
+	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19 },
+	{},
+	{}
+
+local msgNude, msgFullNude, msgClothed, msgFailure =
+	'Nudist: Almost nude now!',
+	'Nudist: Really nude now!',
+	'Nudist: Equipped again. Probably.',
+	'Nudist: |cffFF0000Something went wrong while trying to reequip your weapon slots. Check your equipment!'
 
 local butt = CreateFrame('Button', nil, PaperDollFrame)
-butt:SetFrameStrata('DIALOG')
+butt:SetFrameStrata 'DIALOG'
 butt:SetPoint('BOTTOMRIGHT', CharacterFrame, 'TOPRIGHT', -60, -30)
-butt:SetWidth(64) butt:SetHeight(64)
-butt:SetNormalTexture([[Interface\Addons\Nudist-Again\clothed]])
-butt:SetPushedTexture([[Interface\Addons\Nudist-Again\nude]])
+butt:SetWidth(64)
+butt:SetHeight(64)
+butt:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
+butt:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
 
 local function GetEmpties()
-	for i=0,4 do freeslots[i] = 0 end
-	for bag=0,4 do
-		for slot=1,GetContainerNumSlots(bag) do
+	for i = 0, 4 do
+		freeslots[i] = 0
+	end
+	for bag = 0, 4 do
+		for slot = 1, GetContainerNumSlots(bag) do
 			if not GetContainerItemInfo(bag, slot) then freeslots[bag] = freeslots[bag] + 1 end
 		end
 	end
 end
 
 local function GetNextEmpty()
-	for i=0,4 do
+	for i = 0, 4 do
 		if freeslots[i] > 0 then
 			freeslots[i] = freeslots[i] - 1
 			return i
@@ -41,37 +53,35 @@ local function EquipItemByBagSlot(item, slot)
 end
 
 local function UnequipDone(m)
-	butt:SetNormalTexture([[Interface\Addons\Nudist-Again\nude]])
-	butt:SetPushedTexture([[Interface\Addons\Nudist-Again\clothed]])
+	butt:SetNormalTexture 'Interface/Addons/Nudist-Again/nude'
+	butt:SetPushedTexture 'Interface/Addons/Nudist-Again/clothed'
 	print(m)
 end
 
 local function ReequipDone(m)
 	table.wipe(items)
-	butt:SetNormalTexture([[Interface\Addons\Nudist-Again\clothed]])
-	butt:SetPushedTexture([[Interface\Addons\Nudist-Again\nude]])
+	butt:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
+	butt:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
 	print(m)
 end
 
 
 
 local function handler()
-	
 	--[[ If in combat, we do nothing, also not trying to reequip, bc chances are
 	very high that we get a Protected Function error and end up with an
 	equipment item on the cursor, which is disturbing. ]]
 	if InCombatLockdown() then return end
-	
-	if CursorHasItem() then ClearCursor() end
-	
-	if #items > 0 then
 
+	if CursorHasItem() then ClearCursor() end
+
+	if #items > 0 then
 		for s, i in pairs(items) do
 			EquipItemByName(i, s)
 		end
-		
+
 		local msg, verify = msgClothed, false
-		
+
 		C_Timer.After(0.8, function()
 			for s = 16, 17 do -- Check only weapon slots [^1]
 				if items[s] and not GetInventoryItemLink('player', s) then
@@ -79,7 +89,7 @@ local function handler()
 					verify = true
 				end
 			end
-			if not verify then 
+			if not verify then
 				ReequipDone(msg)
 			else -- Doublecheck if the 2nd slot 16/17 attempt was successful
 				C_Timer.After(0.8, function()
@@ -93,18 +103,16 @@ local function handler()
 				end)
 			end
 		end)
-
 	else
-
 		GetEmpties()
 
 		local slots, msg = sensitiveSlots, msgNude
-		
+
 		if IsModifierKeyDown() then
-			slots, msg = allSlots, msgFullNude 
+			slots, msg = allSlots, msgFullNude
 		end
-		
-		for _,i in ipairs(slots) do
+
+		for _, i in ipairs(slots) do
 			local bag = GetNextEmpty()
 			if not bag then return end
 
@@ -112,9 +120,12 @@ local function handler()
 			if item then
 				table.insert(items, i, item)
 				PickupInventoryItem(i)
-				if bag == 0 then PutItemInBackpack() else PutItemInBag(bag + 19) end
+				if bag == 0 then
+					PutItemInBackpack()
+				else
+					PutItemInBag(bag + 19)
+				end
 			end
-
 		end
 		UnequipDone(msg)
 	end
