37bbefa7ad40ce93615266a4c11b3cfc6890308e
Tom <tflo@users.noreply.github.com>
2022-11-24 20:19:47 +0100

    Coding style changes and other minor stuff

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
-local debug_prefix = 'Nudist Debug:'
 local function dp(...) -- debugprint
 	-- return
-	print(debug_prefix, ...)
+	print('Nudist Debug:', ...)
 end
 
 --[[
@@ -11,33 +10,36 @@ https://github.com/Ketho/wow-ui-source-df/search?q=CONTAINER_BAG_OFFSET
 ]]
 local CONTAINER_BAG_OFFSET = 30
 
-local sensitiveSlots, allSlots, items, freeslots =
+local SENSITIVE_SLOTS, ALL_SLOTS, items, freeslots =
 	{ 1, 3, 5, 6, 7, 8, 9, 10, 16, 17 },
-	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19 },
+	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19 }, -- 18 is no longer used in Retail
 	{},
 	{}
 
-local msgNude, msgFullNude, msgClothed, msgFailure =
-	'Nudist: Almost nude now!',
-	'Nudist: Really nude now!',
-	'Nudist: Equipped again. Probably.',
-	'Nudist: |cffFF0000Something went wrong while trying to reequip your weapon slots. Check your equipment!'
+local MSG_PREFIX, MSG_NUDE, MSG_FULL_NUDE, MSG_CLOTHED, MSG_FAILURE =
+	'Nudist:',
+	'Almost nude now!',
+	'Really nude now!',
+	'Equipped again.',
+	'|cffFF0000Something went wrong while trying to reequip your weapon slots. Check your equipment!'
 
-local butt = CreateFrame('Button', nil, PaperDollFrame)
-butt:SetFrameStrata 'DIALOG'
-butt:SetPoint('BOTTOMRIGHT', CharacterFrame, 'TOPRIGHT', -60, -30)
-butt:SetWidth(64)
-butt:SetHeight(64)
-butt:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
-butt:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
+local function status_msg(...) print(MSG_PREFIX, ...) end
 
-local function GetEmpties()
+local btn = CreateFrame('Button', nil, PaperDollFrame)
+btn:SetFrameStrata 'DIALOG'
+btn:SetPoint('BOTTOMRIGHT', CharacterFrame, 'TOPRIGHT', -60, -30)
+btn:SetWidth(64)
+btn:SetHeight(64)
+btn:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
+btn:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
+
+local function get_empties()
 	for bag = 0, 4 do
 		freeslots[bag] = C_Container.GetContainerNumFreeSlots(bag)
 	end
 end
 
-local function GetNextEmpty()
+local function get_next_empty()
 	for i = 0, 4 do
 		if freeslots[i] > 0 then
 			freeslots[i] = freeslots[i] - 1
@@ -46,30 +48,30 @@ local function GetNextEmpty()
 	end
 end
 
-local function EquipItemByBagSlot(item, slot)
+local function equip_item_by_bagslot(item, slot)
 	for bag = 0, 4 do
 		local max = C_Container.GetContainerNumSlots(bag)
 		for bagslot = 1, max do
 			if C_Container.GetContainerItemLink(bag, bagslot) == item then
 				C_Container.UseContainerItem(bag, bagslot)
-				print("Reequipped missing item via 'UseContainerItem': Slot", slot, item) -- debug
+				status_msg("Reequipped missing item via 'UseContainerItem': Slot", slot, item) -- debug
 				return
 			end
 		end
 	end
 end
 
-local function UnequipDone(m)
-	butt:SetNormalTexture 'Interface/Addons/Nudist-Again/nude'
-	butt:SetPushedTexture 'Interface/Addons/Nudist-Again/clothed'
-	print(m)
+local function unequip_done(m)
+	btn:SetNormalTexture 'Interface/Addons/Nudist-Again/nude'
+	btn:SetPushedTexture 'Interface/Addons/Nudist-Again/clothed'
+	status_msg(m)
 end
 
-local function ReequipDone(m)
+local function reequip_done(m)
 	table.wipe(items)
-	butt:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
-	butt:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
-	print(m)
+	btn:SetNormalTexture 'Interface/Addons/Nudist-Again/clothed'
+	btn:SetPushedTexture 'Interface/Addons/Nudist-Again/nude'
+	status_msg(m)
 end
 
 
@@ -87,40 +89,40 @@ local function handler()
 			EquipItemByName(i, s)
 		end
 
-		local msg, verify = msgClothed, false
+		local msg, verify = MSG_CLOTHED, false
 
 		C_Timer.After(0.8, function()
 			for s = 16, 17 do -- Check only weapon slots [^1]
 				if items[s] and not GetInventoryItemLink('player', s) then
-					EquipItemByBagSlot(items[s], s)
+					equip_item_by_bagslot(items[s], s)
 					verify = true
 				end
 			end
 			if not verify then
-				ReequipDone(msg)
+				reequip_done(msg)
 			else -- Doublecheck if the 2nd slot 16/17 attempt was successful
 				C_Timer.After(0.8, function()
 					for s = 16, 17 do
 						if items[s] and not GetInventoryItemLink('player', s) then
-							msg = msgFailure
+							msg = MSG_FAILURE
 							break
 						end
 					end
-					ReequipDone(msg)
+					reequip_done(msg)
 				end)
 			end
 		end)
 	else
-		GetEmpties()
+		get_empties()
 
-		local slots, msg = sensitiveSlots, msgNude
+		local slots, msg = SENSITIVE_SLOTS, MSG_NUDE
 
 		if IsModifierKeyDown() then
-			slots, msg = allSlots, msgFullNude
+			slots, msg = ALL_SLOTS, MSG_FULL_NUDE
 		end
 
 		for _, i in ipairs(slots) do
-			local bag = GetNextEmpty()
+			local bag = get_next_empty()
 			if not bag then return end
 
 			local item = GetInventoryItemLink('player', i)
@@ -134,13 +136,13 @@ local function handler()
 				end
 			end
 		end
-		UnequipDone(msg)
+		unequip_done(msg)
 	end
 end
 
 SLASH_NUDIST1 = '/nudist'
 SlashCmdList['NUDIST'] = handler
-butt:SetScript('OnClick', handler)
+btn:SetScript('OnClick', handler)
 
 
 
@@ -158,13 +160,13 @@ In that case – for some reason – we cannot equip both items with EquipItemBy
 As a workaround, we have to equip the second one of the identical items via bag slot: we search all bag slots for a matching item link and then equip the item via UseContainerItem(bag, bagslot).
 
 We could use this method right from the start for slots 16 and 17. But I think it's more economic to equip normally, and check afterwards if one of the originally equipped slots is empty. Then we use the workaround for the missing item only. Currently the check is limited to slots 16 and 17, because _I think_ it is impossible to have identical items in the other slots.
-The C_Timer is necessary, otherwise, if the check comes to quickly, we get all slots returned as not equipped. 1 second seems to be safe, probably we can go a bit faster (0.25s though is too short).
+The C_Timer is necessary, otherwise, if the check comes to soon, we get all slots returned as not equipped. 1 second seems to be safe, probably we can go a bit faster (0.25s though is too short).
 
 To check _all_ slots for missing items, use this:
 
 -- 			for s, i in pairs(items) do -- Check all slots [^1]
 -- 				if not GetInventoryItemLink('player', s) then
--- 					EquipItemByBagSlot(i, s)
+-- 					equip_item_by_bagslot(i, s)
 -- 				end
 -- 			end
 
